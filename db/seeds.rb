b158cbfa31c0dfe66aa58903aa2fefb323a4eb70
Nancy <nc.nancy.chan@gmail.com>
2015-09-02 23:04:26 -0700

    Assignment 30 completed, Seed Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,8 @@
 include RandomData
-  50.times do
-    Post.create!(
-      title: RandomData.random_sentence,
-      body: RandomData.random_paragraph
-    )
-  end
-  posts = Post.all
 
-  100.times do
-    Comment.create!(
-      post: posts.sample,
-      body: RandomData.random_paragraph
-    )
-  end
+  Post.find_or_create_by(title: "Title 1") {|post| post.body = "Body 1"}
+  Comment.find_or_create_by(body: "Comment Body 1") {|comment| comment.post = Post.all.sample}
 
 puts "Seed finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+puts "#{Post.count} posts exist"
+puts "#{Comment.count} comments exist"
