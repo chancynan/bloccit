4a4defc88da6a856ba800131dbc1d14f1d4d75e8
Nancy <nc.nancy.chan@gmail.com>
2015-10-01 22:59:05 -0700

    Checkpoint 49 completed, public profiles

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,12 @@ class User < ActiveRecord::Base
   enum role: [:member, :admin]
 
   def favorite_for(post)
-  favorites.where(post_id: post.id).first
+    favorites.where(post_id: post.id).first
   end
+
+  def self.avatar_url(user, size)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+  end
+
 end
