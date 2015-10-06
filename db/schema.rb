d78ef68afd6812ef385d2407cbfe73a2d3c4cc57
Nancy <nc.nancy.chan@gmail.com>
2015-10-05 20:19:20 -0700

    regenerated schema

--- db/schema.rb
+++ db/schema.rb
@@ -72,31 +72,6 @@ ActiveRecord::Schema.define(version: 20150928041714) do
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
-  create_table "rating_associations", force: :cascade do |t|
-    t.integer  "rating_id"
-    t.integer  "topic_id"
-    t.integer  "post_id"
-    t.integer  "rateable_id"
-    t.string   "rateable_type"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
-
-  add_index "rating_associations", ["post_id"], name: "index_rating_associations_on_post_id"
-  add_index "rating_associations", ["rateable_type", "rateable_id"], name: "index_rating_associations_on_rateable_type_and_rateable_id"
-  add_index "rating_associations", ["rating_id"], name: "index_rating_associations_on_rating_id"
-  add_index "rating_associations", ["topic_id"], name: "index_rating_associations_on_topic_id"
-
-  create_table "ratings", force: :cascade do |t|
-    t.string   "severity"
-    t.integer  "rateable_id"
-    t.string   "rateable_type"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
-
-  add_index "ratings", ["rateable_type", "rateable_id"], name: "index_ratings_on_rateable_type_and_rateable_id"
-
   create_table "topics", force: :cascade do |t|
     t.string   "name"
     t.boolean  "public",      default: true
