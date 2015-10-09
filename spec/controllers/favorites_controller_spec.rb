f21fbf49158d463a405164da01587a7e7e65c7e9
Nancy <nc.nancy.chan@gmail.com>
2015-10-09 13:49:28 -0700

    Fixed test

--- spec/controllers/favorites_controller_spec.rb
+++ spec/controllers/favorites_controller_spec.rb
@@ -36,7 +36,6 @@ RSpec.describe FavoritesController, type: :controller do
          end
 
          it 'creates a favorite for the current user and specified post' do
-           expect(my_user.favorites.find_by_post_id(my_post.id)).to be_nil
            post :create, { post_id: my_post.id }
            expect(my_user.favorites.find_by_post_id(my_post.id)).not_to be_nil
          end
@@ -51,9 +50,8 @@ RSpec.describe FavoritesController, type: :controller do
 
          it 'destroys the favorite for the current user and post' do
            favorite = my_user.favorites.where(post: my_post).create
-           expect( my_user.favorites.find_by_post_id(my_post.id) ).not_to be_nil
            delete :destroy, { post_id: my_post.id, id: favorite.id }
-           expect( my_user.favorites.find_by_post_id(my_post.id) ).to be_nil
+           expect(my_user.favorites.find_by_post_id(my_post.id)).to be_nil
          end
        end
      end
